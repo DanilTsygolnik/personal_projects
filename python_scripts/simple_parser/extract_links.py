c1ce940a3d0decb3217e99caab484f87218e1d87
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-05-02 23:00:23 +0300

    parser: get filenames with numbers (success)

--- python_scripts/simple_parser/extract_links.py
+++ python_scripts/simple_parser/extract_links.py
@@ -7,10 +7,13 @@ topics_filenames_list = []
 with open(src_html_file) as fp:
     soup = BeautifulSoup(fp, 'html.parser')
     a_blocks = soup.find_all('a', 'text-decoration-none link-dark me-2')
+    link_num = 1
     for link in a_blocks:
         relative_url = link.get('href')
         full_url = "".join(["https://ru.hexlet.io", relative_url])
         urls_list.append(full_url)
         topic = link.get_text()  # Example: "Большая сложная тема"
         filename_for_topic = "_".join(topic.split())   # Result: "Большая_сложная_тема"
-        topics_filenames_list.append(filename_for_topic)
+        filename_with_num = "_".join([str(link_num), filename_for_topic])
+        topics_filenames_list.append(filename_with_num)
+        link_num += 1
