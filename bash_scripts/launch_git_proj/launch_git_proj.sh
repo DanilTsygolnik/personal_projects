15fb2cdcaa27d99723e5fded3e90a57a9c2d4057
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-05-02 14:28:32 +0300

    script: message for user is not quite right

---
+++ bash_scripts/launch_git_proj/launch_git_proj.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git_projects_path="$HOME/Documents/current_work/programming/git_projects/"
+projects_list="$(ls -1 $git_projects_path)"
+message="You have the following programming projects:\n$projects_list\nWhich one you want to work work with? Enter the name: "
+read -p "$(printf '%s\n' "$message")" project_name
+project_dir="$git_projects_path$project_name"
+# connect to github
+xfce4-terminal --tab --title=git --working-directory=$project_dir --execute bash -c 'eval $(ssh-agent) -s; ssh-agent -s; ssh-add $HOME/.ssh/git_key; ssh -T git@github.com; exec bash'
+# new tab for editing code
+xfce4-terminal --tab --title=vim --working-directory=$project_dir
+# new tab for testing code
+xfce4-terminal --tab --title=tests --working-directory=$project_dir
