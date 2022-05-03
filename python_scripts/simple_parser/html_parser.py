5e752f07e75b23d3777bc7aaf4e27f7a3d284700
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-05-03 09:43:39 +0300

    parser: syntax errors fixed; refactoring

--- python_scripts/simple_parser/html_parser.py
+++ python_scripts/simple_parser/html_parser.py
@@ -2,18 +2,11 @@ import requests
 import hashlib
 
 
-def get_file_name(parameters:list) -> str:
-    file_name = "parse_result.html"
-    if "-o" in parameters:
-        file_name = "".join([parameters[2], ".html"])
-    return file_name
-
-input_str = str(input())
-parameters = input_str.split()
-my_url = parameters[0]
-file_name = get_file_name(parameters)
-response = requests.get(my_url)
-with open(file_name, 'w') as file:
+input_data = str(input()).split()
+url = input_data[0]
+downloaded_html_full_path = input_data[1]  # example: /home/user/dir/webpage.html
+response = requests.get(url)
+with open(downloaded_html_full_path, 'w') as file:
     file.write(response.text)
 # downloaded html integrity check
 md5_response = hashlib.md5(response.text.encode())
@@ -23,6 +16,6 @@ with open(downloaded_html_full_path, "rb") as f:
         md5_file.update(chunk)
 status_ok = (md5_response.hexdigest() == md5_file.hexdigest())
 if status_ok:
-    print(f'Downloaded {filename} -- OK')
+    print(f'Downloaded {downloaded_html_full_path} -- OK')
 else:
-    print(f'Downloaded {filename} -- HASH ERROR')
+    print(f'Downloaded {downloaded_html_full_path} -- HASH ERROR')
