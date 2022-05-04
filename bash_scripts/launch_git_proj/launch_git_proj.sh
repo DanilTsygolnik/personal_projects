bdd3811e6bea628d4f7ceca025ecdf66b4a39010
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-05-04 10:23:16 +0300

    script: termdown's not responding (fixed)

--- bash_scripts/launch_git_proj/launch_git_proj.sh
+++ bash_scripts/launch_git_proj/launch_git_proj.sh
@@ -5,8 +5,8 @@ message_templ=("You have the following programming projects:" $projects_list "")
 printf '%s\n' "${message_templ[@]}"
 read -p "Which one you want to work work with? Enter the name: " project_name
 project_dir="$git_projects_path$project_name"
-# new tab with termdown set on pause
-xfce4-terminal --tab --execute bash -c 'echo " " | termdown'
+# new tab with termdown running in counter mode
+xfce4-terminal --tab --execute termdown
 # connect to github
 xfce4-terminal --tab --title=git --working-directory=$project_dir --execute bash -c 'eval $(ssh-agent) -s; ssh-agent -s; ssh-add $HOME/.ssh/git_key; ssh -T git@github.com; exec bash'
 # new tab for editing code
